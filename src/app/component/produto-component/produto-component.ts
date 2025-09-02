a2a75e4360c7a33ddb8e3ba2e04dfba57ef95ed9
vagnertondelo <vagnertondello@gmail.com>
2025-09-02 19:00:17 -0300

    produto componente

--- src/app/component/produto-component/produto-component.ts
+++ src/app/component/produto-component/produto-component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProdutoService } from '../../services/produto-service';
+import { ProdutoModel } from '../../models/produtoModel';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-produto-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './produto-component.html',
   styleUrl: './produto-component.css'
 })
-export class ProdutoComponent {
+export class ProdutoComponent implements OnInit {
+
+  private service = inject(ProdutoService);
+
+  produtos: ProdutoModel[]=[];
+  novoNome = '';
+
+  ngOnInit(){
+    this.carregar();
+  }
+
+  carregar(){
+    this.produtos = this.service.listar();
+  }
+
+  adicionar(){
+    const nome= this.novoNome.trim();
+    if(!nome) return;
+    this.service.adicionar(nome);
+    this.novoNome ='';
+    this.carregar();
+  }
+
+    remover(id: number){
+      this.service.remover(id);
+      this.carregar();
+    }
+
+
 
 }
